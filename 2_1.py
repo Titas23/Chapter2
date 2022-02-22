7481f92e1a723b0825564215c9aa7546b7b860c1
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-02-22 14:16:55 -0800

    intial commit

---
+++ 2_1.py
@@ -0,0 +1,36 @@
+# print(3+2, 3-2, 3*2)
+# print(8%2, 8**2, 2*(3+4))
+
+
+# speed = 50
+# time_elasped = 14
+# distance = speed * time_elasped
+# print('The distance is = ',distance)
+
+
+# x = 3.4
+# y = 59
+
+# c = 4*x
+
+# d = 1-c
+# value = x * y
+# print(round(value, 2))
+# print(d)
+
+# num = 5
+
+
+
+total_inches = 41
+feet = total_inches // 12
+inches = total_inches % 12
+print(feet, inches)
+
+centimeters = 0 // 10
+print(centimeters)
+
+import calendar
+year = 2022
+month = 7
+print(calendar.month(year, month))

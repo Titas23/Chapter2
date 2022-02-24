9202fdc84bb04eb32f145d171fc23ac273c7f3f9
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-02-24 15:30:57 -0800

    2_3.py Thursday

--- 2_3.py
+++ 2_3.py
@@ -1,7 +1,19 @@
 
 
-print("hello", "world", sep = "**")
-print("hello", "world", sep = "\n")
-print("1", "two", 3, sep = "  ")
+# print("hello", "world", sep = "**")
+# print("hello", "world", sep = "\n")
+# print("1", "two", 3, sep = " ")
 
 
+# print("hello", end= "\n")
+# print("world")
+
+# print("01234567890123456")
+# print("\"a\tb\tc\"")
+# print("a\tb\tc".expandtabs(5))
+# print("Nudge, \tnudge, \nwink, \twink.".expandtabs(11))
+
+print("Rank".ljust(5), "Player".ljust(20), "HR".rjust(3), sep="")
+print('1'.center(5), "Barry Bonds".ljust(20), "762".rjust(3), sep="")
+print('2'.center(5), "Hank Aaron".ljust(20), "755".rjust(3), sep="")
+print('3'.center(5), "Babe Ruth".ljust(20), "714".rjust(3), sep="")

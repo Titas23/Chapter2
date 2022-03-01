622768085d7bc91d2219334d6aed18271077f44c
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-03-01 15:33:36 -0800

    updated

---
+++ 2_4.py
@@ -0,0 +1,32 @@
+# team = ["Seahawks", 2014, "Century Field"]
+
+# nums = [5, 10, 4, 5]
+
+# words = ["spam", 'ni']
+
+# print(team)
+# print(max(nums))
+# print(words)
+
+num = [200, 300, 400]
+
+# print(sum(num))
+
+# num1 = 1 
+# num2 = 10 
+# num3 = 1000
+
+# sum_of_all_number = num1+num2+num3
+
+# print("Sum is {0:n}".format(sum_of_all_number))
+
+list_of_numbers = [200, 300, 400, 50000, 503530, 305350]
+
+print(sum(list_of_numbers))
+print(max(list_of_numbers))
+print(min(list_of_numbers))
+print(list_of_numbers.count(200))
+print(list_of_numbers.index(200))
+print(list_of_numbers.reverse())
+
+

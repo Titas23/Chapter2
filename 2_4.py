c42c952d65115532f86a799cf87776f50f1c1533
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-03-08 15:04:36 -0800

    2_4 Tuesday

--- 2_4.py
+++ 2_4.py
@@ -8,7 +8,7 @@
 # print(max(nums))
 # print(words)
 
-num = [200, 300, 400]
+# num = [200, 300, 400]
 
 # print(sum(num))
 
@@ -20,13 +20,54 @@ num = [200, 300, 400]
 
 # print("Sum is {0:n}".format(sum_of_all_number))
 
-list_of_numbers = [200, 300, 400, 50000, 503530, 305350]
-
-print(sum(list_of_numbers))
-print(max(list_of_numbers))
-print(min(list_of_numbers))
-print(list_of_numbers.count(200))
-print(list_of_numbers.index(200))
-print(list_of_numbers.reverse())
+from ctypes.wintypes import LPWIN32_FIND_DATAA
 
 
+list_of_numbers = [200, 300, 400, ]
+
+# print(sum(list_of_numbers))
+# print(max(list_of_numbers))
+# print(min(list_of_numbers))
+# print(list_of_numbers.count(200))
+# print(list_of_numbers.index(200))
+# print(list_of_numbers.reverse())
+# list_of_numbers.clear()
+# list_of_numbers.append(3)
+# list_of_numbers.extend([2,3,4])
+
+# print(list_of_numbers)
+
+
+# list_of_numbers2 = [3,4]
+
+# list_of_numbers.extend(list_of_numbers2)
+
+# print(list_of_numbers, "The extend")
+
+# l1 = [1,24,56,54]
+# l2 = [343,645,647,45]
+# l3 = l1 + l2
+
+# del l3[-1]
+# l3.insert(2,"Hello")
+# print(l3, "using the plus sign")
+
+grades = []
+num = float(input("Enter the first grade: "))
+grades.append(num)
+num = float(input("Enter the second grade: "))
+grades.append(num)
+num = float(input("Enter the Third grade: "))
+grades.append(num)
+num = float(input("Enter the fourth grade: "))
+grades.append(num)
+num = float(input("Enter the fifth grade: "))
+grades.append(num)
+
+
+minimum_grade = min(grades)
+grades.remove(minimum_grade)
+minimum_grade = min(grades)
+grades.remove(minimum_grade)
+average = sum(grades) / len(grades)
+print("Average grade: {0:2f}".format(average))

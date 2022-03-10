8e208cc1015d062926f93f86fa4880feba31dd79
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-03-10 15:25:03 -0800

    chapter2_4

--- 2_4.py
+++ 2_4.py
@@ -23,7 +23,7 @@
 from ctypes.wintypes import LPWIN32_FIND_DATAA
 
 
-list_of_numbers = [200, 300, 400, ]
+list_of_numbers = [200, 300, 400]
 
 # print(sum(list_of_numbers))
 # print(max(list_of_numbers))
@@ -52,22 +52,69 @@ list_of_numbers = [200, 300, 400, ]
 # l3.insert(2,"Hello")
 # print(l3, "using the plus sign")
 
-grades = []
-num = float(input("Enter the first grade: "))
-grades.append(num)
-num = float(input("Enter the second grade: "))
-grades.append(num)
-num = float(input("Enter the Third grade: "))
-grades.append(num)
-num = float(input("Enter the fourth grade: "))
-grades.append(num)
-num = float(input("Enter the fifth grade: "))
-grades.append(num)
+# grades = []
+# num = float(input("Enter the first grade: "))
+# grades.append(num)
+# num = float(input("Enter the second grade: "))
+# grades.append(num)
+# num = float(input("Enter the Third grade: "))
+# grades.append(num)
+# num = float(input("Enter the fourth grade: "))
+# grades.append(num)
+# num = float(input("Enter the fifth grade: "))
+# grades.append(num)
 
 
-minimum_grade = min(grades)
-grades.remove(minimum_grade)
-minimum_grade = min(grades)
-grades.remove(minimum_grade)
-average = sum(grades) / len(grades)
-print("Average grade: {0:2f}".format(average))
+# minimum_grade = min(grades)
+# grades.remove(minimum_grade)
+# minimum_grade = min(grades)
+# grades.remove(minimum_grade)
+# average = sum(grades) / len(grades)
+# print("Average grade: {0:2f}".format(average))
+
+
+# letters = "a,b,c"
+# parts = letters.split(",")
+
+# print(letters)
+
+# lines = ["To", "be", "or", "not", "to", "be"]
+
+# print("lines before join", lines)
+
+# joined = " ".join(lines)
+
+# print("joined: ", joined)
+
+
+
+# Open up the data.txt file in read mode
+
+# infile = open("data.txt", 'r')
+# # Using the for loop to poppulate the names array with names
+# names = []
+# for line in infile:
+#     names.append(line.rstrip())
+# print("Names: ",names)
+# infile.close()
+
+# infile = open("data.txt", 'r')
+# names_using_list_comprehension = [line.rstrip() for line in infile]
+# print(names_using_list_comprehension)
+# # Close the data.txt file to preserve memory
+# infile.close()
+
+# infile = open("grades.txt", 'r')
+# list_of_numbers = [eval(line) for line in infile]
+# print(list_of_numbers)
+
+# infile.close()
+
+# list_of_names = ("lucas", "John", ["A", "B", "C"])
+
+# print(list_of_names[2][1][-1])
+
+list1 = ["A", "B", "C"]
+list2 = list(list1) 
+list2.append("D")
+print(list1)
